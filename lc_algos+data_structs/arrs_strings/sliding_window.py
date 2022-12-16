ec8950cf0ce4d8dc056cbd42a5395b476fe37ff3
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-12-16 12:58:46 +0100

    Update sliding window, example 1

--- lc_algos+data_structs/arrs_strings/sliding_window.py
+++ lc_algos+data_structs/arrs_strings/sliding_window.py
@@ -1,22 +1,28 @@
+from typing import List
+
 """
 Example 1: Given an array of positive integers nums and an integer k, find the length of the longest subarray whose sum is less than or equal to k.
 """
 
 
-def find_length(nums, k):
-    """Takes array of positive integers nums and an integer k, finds the length of the longest subarray whose sum is less than or equal to k."""
-    left = curr = ans = 0
-    for right in range(len(nums)):
-        curr += nums[right]
-        while curr > k:
-            curr -= nums[left]
+def find_longest_subarray(list_nums: List[int], constraint: int) -> int:
+    """Takes array of positive integers nums and an integer k, finds the length of the longest subarray whose sum is less than or equal to k.
+    Time complexity: O(n)
+    Space complexity: 0(1)
+    """
+    left = sum_curr_window = longest = 0
+    for right in range(len(list_nums)):
+        sum_curr_window += list_nums[right]
+        while sum_curr_window > constraint:
+            # shrink window from left
+            sum_curr_window -= list_nums[left]
             left += 1
-        ans = max(ans, right - left + 1)
+        longest = max(longest, right - left + 1)
 
-    return ans
+    return longest
 
 
 if __name__ == "__main__":
     arr1 = [3, 1, 2, 7, 4, 2, 1, 1, 5]
     lim1 = 8
-    print(find_length(arr1, lim1))
+    print(find_longest_subarray(arr1, lim1))
