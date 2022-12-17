abb876cfced69d853c9d97982177690f8df28d6c
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-12-17 06:13:51 +0100

    Sliding window with binary string problem

--- lc_algos+data_structs/arrs_strings/sliding_window.py
+++ lc_algos+data_structs/arrs_strings/sliding_window.py
@@ -22,6 +22,33 @@ def find_longest_subarray(list_nums: List[int], constraint: int) -> int:
     return longest
 
 
+"""
+2: You are given a binary substring s (a string containing only "0" and "1"). An operation involves flipping a "0" into a "1". What is the length of the longest substring containing only "1" after performing at most one operation?
+
+For example, given s = "1101100111", the answer is 5. If you perform the operation at index 2, the string becomes 1111100111.
+
+"""
+
+
+def find_longest_ones(s: str) -> int:
+    """
+    Given max one operation involves flipping a "0" into a "1", what is the length of the longest substring containing only "1"?
+
+    For example, given s = "1101100111", the answer is 5. If you perform the operation at index 2, the string becomes 1111100111.
+    """
+    left = current = ans = 0
+    for right in range(len(s)):
+        if s[right] == "0":
+            current += 1
+        while current > 1:
+            if s[left] == "0":
+                current -= 1
+            left += 1
+        ans = max(ans, right - left + 1)
+
+    return ans
+
+
 if __name__ == "__main__":
     arr1 = [3, 1, 2, 7, 4, 2, 1, 1, 5]
     lim1 = 8
--- problems_list.py
+++ problems_list.py
@@ -36,5 +36,11 @@ After sorting, it becomes [0,1,9,16,100].
 
 """ ================== Sliding Window =================="""
 """
-Example 1: Given an array of positive integers nums and an integer k, find the length of the longest subarray whose sum is less than or equal to k.
+1: Given an array of positive integers nums and an integer k, find the length of the longest subarray whose sum is less than or equal to k.
+"""
+"""
+2: You are given a binary substring s (a string containing only "0" and "1"). An operation involves flipping a "0" into a "1". What is the length of the longest substring containing only "1" after performing at most one operation?
+
+For example, given s = "1101100111", the answer is 5. If you perform the operation at index 2, the string becomes 1111100111.
+
 """
